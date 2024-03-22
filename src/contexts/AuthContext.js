f91015c1e49fc3cf66959ec85c442a33f02cfcb4
Praveen <praveensingh55602@gmail.com>
2024-03-22 19:54:26 +0530

    auth context created

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCOJjM4P-4w_o9aIABZpy_bcCDAMpapg7o",
+    authDomain: "auth-development-1107a.firebaseapp.com",
+    projectId: "auth-development-1107a",
+    storageBucket: "auth-development-1107a.appspot.com",
+    messagingSenderId: "709789744441",
+    appId: "1:709789744441:web:619927750743a13c7b3ce8",
+  });
+  const auth = getAuth(firebaseApp);
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+    loading,
+    setLoading,
+    auth,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
